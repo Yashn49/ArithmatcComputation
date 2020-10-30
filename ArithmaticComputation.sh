61cdd2d6abb751a778e33240617803e8923e5142
Yash <yashn49@gmail.com>
2020-10-31 02:44:09 +0530

    Calculation1

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -4,3 +4,5 @@ echo "Welcome to arithmatic computation and sorting program"
 read -p "Enter the value 1 : " a
 read -p "Enter the value 2 : " b
 read -p "Enter the value 3 : " c
+calculate=$((a + (b * c)))
+echo $calculate
